b5842e474fdfd34381ca036e91505b33b45572f6
dev_demir <dev_demir@outlook.com>
2022-11-08 01:10:41 +0300

    Initial Commit

---
+++ GuessTheFlag-BetaV1.0/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GuessTheFlag-BetaV1.0
+//
+//  Created by Emrullah Demir on 8.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheFlag-BetaV1.0/GuessTheFlag_BetaV1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag_BetaV1_0App.swift
+//  GuessTheFlag-BetaV1.0
+//
+//  Created by Emrullah Demir on 8.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlag_BetaV1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
